dfe7336ab096153145671986435048efdc49885d
colorfulgreen <colorfulgreen@gmail.com>
2020-04-16 14:49:25 +0800

    2-4.trackbar

---
+++ ch02/2-4.trackbar.cpp
@@ -0,0 +1,59 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int g_slider_position = 0;
+
+int g_run = 1; // positive number displayed before stopping, negtive number means continuous mode
+int g_dont_single_step = 0; // update the trackbar without triggering single-step mode
+cv::VideoCapture g_cap;
+
+void onTrackbarSlide(int pos, void *) {
+    g_cap.set(cv::CAP_PROP_POS_FRAMES, pos);
+    if(!g_dont_single_step) 
+        g_run = 1;
+    g_dont_single_step = 0;
+}
+
+int main(int argc, char** argv) {
+    cv::namedWindow("Example2_4", cv::WINDOW_AUTOSIZE);
+    g_cap.open(string(argv[1]));
+    int frames = (int) g_cap.get(cv::CAP_PROP_FRAME_COUNT); // TODO prop
+    int tmpw = (int) g_cap.get(cv::CAP_PROP_FRAME_WIDTH);
+    int tmph = (int) g_cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+    cout << "Video has " << frames << " frames of dimensions("
+         << tmpw << ", " << tmph << ")." << endl;
+
+    cv::createTrackbar("Position", "Example2_4", &g_slider_position, frames,
+            onTrackbarSlide);
+
+    cv::Mat frame;
+    for(;;) {
+        if(g_run != 0) {
+            g_cap >> frame; if(frame.empty()) break;
+            int current_pos = (int)g_cap.get(cv::CAP_PROP_POS_FRAMES);
+            g_dont_single_step = 1; 
+
+            cv::setTrackbarPos("Position", "Example2_4", current_pos);
+            cv::imshow("Example2_4", frame);
+
+            g_run -= 1;
+        }
+        char c =  (char) cv::waitKey(1);
+        if( c == 's' ) {
+            g_run = 1;
+            cout << "Single step, run = " << g_run << endl;
+        }
+        if( c == 'r' ) {
+            g_run = -1;
+            cout << "Run mode, run = " << g_run << endl;
+        }
+        if( c == 27 )
+            break;
+    }
+
+    return 0;
+}
